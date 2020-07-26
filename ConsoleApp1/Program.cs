561c9812f0d3cf8d48e60783b95b39dd2226238c
atefeh.zng <atefeh.zng@outlook.com>
2020-07-26 17:58:10 +0430

    adding my heap project to github

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace piyade_sazi_heap
+{
+	class Program
+	{
+		static void maxheap(int size)
+		{
+			for(int j=0;j<size;j++)
+			{
+
+			}
+		}
+		static void Main(string[] args)
+		{
+			int size = Convert.ToInt32(Console.ReadLine());
+			int[] input = new int[size];
+			for (int i = 0; i < size; i++)
+			{
+				input[i] = Convert.ToInt32(Console.ReadLine());
+			}
+			//Console.WriteLine("Hello World!");
+		}
+	}
+}
